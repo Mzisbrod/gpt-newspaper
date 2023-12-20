8216ab2d6be3d9e5aa1ccc14af9cdf42d2597783
Rotem B. Weiss <91344214+rotemweiss57@users.noreply.github.com>
2023-12-20 16:03:55 +0200

    update frontend

--- frontend/static/scripts.js
+++ frontend/static/scripts.js
@@ -1,16 +1,20 @@
 function produceNewspaper() {
     var topics = [];
-    for (var i = 1; i <= 4; i++) {
+    for (var i = 1; i <= topicCount; i++) {
         var topic = document.getElementById('topic' + i).value.trim();
         if (topic) {
             topics.push(topic);
         }
     }
 
-    if (topics.length < 4) {
-        alert('Please fill in all four topics.');
+    if (topics.length === 0) {
+        alert('Please fill in at least one topic.');
         return;
     }
+
+        // Show loading animation
+    toggleLoading(true);
+
 
     fetch('http://localhost:8000/generate_newspaper', {
         method: 'POST',
@@ -21,22 +25,91 @@ function produceNewspaper() {
     })
     .then(response => response.json())
     .then(data => {
-        console.log('Success:', data);
-        // Process and display the newspaper data here
+        toggleLoading(false);
         displayNewspaper(data);
     })
     .catch((error) => {
+        toggleLoading(false);
         console.error('Error:', error);
-
     });
 }
+
+function toggleLoading(isLoading) {
+    const loadingSection = document.getElementById('loading');
+    const loadingMessages = document.getElementById('loadingMessages');
+    const messages = ["Looking for news...", "Curating sources...", "Writing articles...", "Editing final newspaper..."];
+    if (isLoading) {
+        loadingSection.classList.remove('hidden');
+        let messageIndex = 0;
+        loadingMessages.textContent = messages[messageIndex];
+        const interval = setInterval(() => {
+            if (messageIndex < messages.length - 1) {
+                messageIndex++;
+                loadingMessages.textContent = messages[messageIndex];
+            } else {
+                clearInterval(interval);
+            }
+        }, 6000);
+        loadingSection.dataset.intervalId = interval;
+    } else {
+        loadingSection.classList.add('hidden');
+        clearInterval(loadingSection.dataset.intervalId);
+    }
+}
+
+
+let topicCount = 1;
+
 window.addEventListener('DOMContentLoaded', (event) => {
     document.getElementById('produceNewspaper').addEventListener('click', produceNewspaper);
+    addIconToLastTopic();
 });
 
+function addIconToLastTopic() {
+    // Remove icons from all topics
+    document.querySelectorAll('.add-topic, .remove-topic').forEach(icon => {
+        icon.remove();
+    });
+
+    // Add icons to the last topic only
+    const lastTopic = document.getElementById('topicGroup' + topicCount);
+    if (lastTopic) {
+        const addIcon = document.createElement('span');
+        addIcon.className = 'icon add-topic';
+        addIcon.textContent = '+';
+        addIcon.addEventListener('click', addTopicField);
+        lastTopic.appendChild(addIcon);
+
+        if (topicCount > 1) {
+            const removeIcon = document.createElement('span');
+            removeIcon.className = 'icon remove-topic';
+            removeIcon.textContent = '🗑️';
+            removeIcon.addEventListener('click', removeTopicField);
+            lastTopic.appendChild(removeIcon);
+        }
+    }
+}
+
+function addTopicField() {
+    topicCount++;
+    const formGroup = document.createElement('div');
+    formGroup.className = 'form-group';
+    formGroup.id = 'topicGroup' + topicCount;
+    formGroup.innerHTML = `<input type="text" id="topic${topicCount}" name="topic${topicCount}" required>`;
+    document.getElementById('topicForm').appendChild(formGroup);
+    addIconToLastTopic();
+}
+
+function removeTopicField(event) {
+    const topicGroup = event.target.parentElement;
+    if (topicGroup && topicGroup.id !== 'topicGroup1') {
+        topicGroup.remove();
+        topicCount--;
+        addIconToLastTopic();
+    }
+}
 
 function displayNewspaper(data) {
-    // Assuming 'data.path' contains the URL/path to the generated newspaper
     if (data.path) {
         window.location.href = data.path;
     } else {
